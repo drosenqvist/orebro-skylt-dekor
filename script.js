5a21640fe1cc6a118980fadd7605ee2dd29378b0
Daniel Rosenqvist <daniel.wibergrosenqvist@gmail.com>
2020-07-23 20:53:20 +0200

    Major refactoring

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const init = () => {
 }
 
 const renderEmployees = (jsonResponse) => {
-  const employeeContainer = document.querySelector('.container'),
+  const employeeContainer = document.querySelector('.employee-container'),
         employeeList = jsonResponse.employees
 
   employeeList.forEach(function(employee) {
@@ -16,8 +16,14 @@ const renderEmployees = (jsonResponse) => {
     image.src = employee.image
     h3.textContent = employee.name
     position.textContent = employee.position
-    phone.textContent = employee.phone
-    email.textContent = employee.email
+
+    if (employee.phone) {
+      phone.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="phone" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"/><rect x="7" y="4" width="10" height="16" rx="1" /><line x1="11" y1="5" x2="13" y2="5" /><line x1="12" y1="17" x2="12" y2="17.01" /></svg>` + employee.phone
+    }
+
+    if (employee.email) {
+      email.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="email" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"/><rect x="3" y="5" width="18" height="14" rx="2" /><polyline points="3 7 12 13 21 7" /></svg>` + employee.email
+    }
 
     const container = document.createElement('div')
 
@@ -92,32 +98,24 @@ const imageHandler = (index) => {
     return false
   }
 
-  const imageContainer = document.querySelector('.modal-container'),
-        modalHolder = document.querySelector('.modal-holder'),
+  const modalHolder = document.querySelector('.modal-holder'),
         modal = document.createElement('img')
 
   modalHolder.className = 'modal-holder-active'
-  imageContainer.className = 'modal-container-active'
   modal.src = index.src
 
-  // Increase size of smaller images
-  if (index.height < 300) {
-    imageContainer.classList.add('big-modal-image')
-  }
+  modalHolder.appendChild(modal)
 
-  imageContainer.appendChild(modal)
-
-  imageModalControl(imageContainer, modalHolder)
+  imageModalControl(modalHolder)
 }
 
 
-const imageModalControl = (imageContainer, modalHolder) => {
-  const closeModal = document.querySelector('.close-modal-container'),
-        image = document.querySelector('.modal-container-active').children[1]
+const imageModalControl = (modalHolder) => {
+  const closeModal = document.querySelector('.close-modal'),
+        image = document.querySelector('.modal-holder-active').children[1]
 
   const close = () => {
     image.remove()
-    imageContainer.className = 'modal-container'
     modalHolder.className = 'modal-holder'
   }
 
@@ -147,7 +145,7 @@ const imageFilter = () => {
 
         categories.forEach(function(category) {
           category.addEventListener('click', () => {
-            if (category.checked === true) {
+            if (category.checked) {
               matchWithImages(category)
             }
           })
